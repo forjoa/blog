6e3dbc5acec424a8f145f3fc0dced96b9364d764
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-19 18:44:31 +0100

    add browser route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 // components
 import Home from './components/Home'
 
+// imports 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
