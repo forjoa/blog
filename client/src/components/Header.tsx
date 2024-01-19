70b316e8f0f43cbc4cf70f1d9ab644b254902256
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-19 20:00:02 +0100

    create and add header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,27 @@
+import { Link } from 'react-router-dom'
 import logo from '../assets/blog-icon.png'
+import '../styles/header.css'
 
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt='Logo icon' />
-      <nav>
-        <a href="/">Home</a>
-        <a href="/search">Search</a>
-      </nav>
+      <div className='header-container'>
+        <img src={logo} alt='Logo icon' />
+        <nav>
+          <div className='left-nav'>
+            <Link to='/'>Home</Link>
+            <Link to='/search'>Search</Link>
+          </div>
+          <div className='right-nav'>
+            <Link to='/login' className='login-btn'>
+              Login
+            </Link>
+            <Link to='/register' className='register-btn'>
+              Try it now
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   )
 }
