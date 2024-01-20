83c606d7573564cb2469105925c4d2791dc86464
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-20 19:14:43 +0100

    create login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import Home from './components/Home'
 
 // imports 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Login.tsx
@@ -0,0 +1,9 @@
+import Header from "./Header"
+
+const Login = () => {
+    return (
+        <Header></Header>
+    )
+}
+
+export default Login
