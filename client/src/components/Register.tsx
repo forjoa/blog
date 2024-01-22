0c5b6753fe91731f4481945394f06bf94fdf4a09
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-22 13:11:20 +0100

    register form made correctly

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,9 +1,84 @@
+import { useState } from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import { IconEye, IconEyeOff } from '@tabler/icons-react'
 
 const Register = () => {
-    return (
-        <>
-        </>
-    )
+  const [name, setName] = useState<string>('')
+  const [username, setUsername] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [seePwd, setSeePwd] = useState<boolean>(false)
+
+  return (
+    <>
+      <Header />
+      <div className='main-login'>
+        <form>
+          <h1>Register</h1>
+          <div className='name-container'>
+            <label htmlFor='name'>Full name</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              placeholder='Name Lastname'
+              autoComplete='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className='username-container'>
+            <label htmlFor='username'>Username</label>
+            <input
+              type='text'
+              name='username'
+              id='username'
+              placeholder='Username'
+              autoComplete='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className='email-container'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              placeholder='example@email.com'
+              autoComplete='username'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className='password-container'>
+            <label htmlFor='password'>Password</label>
+            <div className='input-pwd-container'>
+              <input
+                type={seePwd ? 'text' : 'password'}
+                name='password'
+                id='password'
+                autoComplete='current-password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {seePwd ? (
+                <IconEyeOff onClick={() => setSeePwd(!seePwd)} />
+              ) : (
+                <IconEye onClick={() => setSeePwd(!seePwd)} />
+              )}
+            </div>
+          </div>
+
+          <input type='submit' value='Register' className='login-submit' />
+        </form>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Register
