b4a0da43b5a8070a1659ba3e7dc590037ce7e4aa
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-23 09:18:43 +0100

    add input to search blogs

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,9 +1,34 @@
 import Header from './Header'
+import '../styles/search.css'
+import { useState } from 'react'
+import { IconSearch } from '@tabler/icons-react'
 
 const Search = () => {
+  const [searching, setSearching] = useState<string>('')
+
   return (
     <>
       <Header />
+      <div className='main-search'>
+        <div className='main-search-background'></div>
+        <div className='main-search-container'>
+          <form>
+            <input
+              type='text'
+              name='searching'
+              id='searching'
+              spellCheck='false'
+              autoComplete='off'
+              placeholder='Search anything you want...'
+              value={searching}
+              onChange={(e) => setSearching(e.target.value)}
+            />
+            <button type='submit'>
+              <IconSearch color='black' />
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
