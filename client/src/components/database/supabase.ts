eb4d496a44dac1be79f73fcd1a3ab931a8ad4d67
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-29 11:13:53 +0100

    one more try

--- client/src/components/database/supabase.ts
+++ client/src/components/database/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl : string = import.meta.env.VITE_SUPABASE_URL ?? 'https://qryxnxryvjnrbgfroqrp.supabase.co'
-const supabaseKey : string = process.env.API_KEY ?? import.meta.env.VITE_API_KEY 
+const supabaseKey : string = import.meta.env.VITE_API_KEY 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
