1082d757573f1902dc1fa4ccc95705fd427deb51
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-29 10:26:55 +0100

    change again enviroment variable

--- client/src/components/database/supabase.ts
+++ client/src/components/database/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl : string = import.meta.env.VITE_SUPABASE_URL ?? import.meta.env.SUPABASE_URL 
+const supabaseUrl : string = import.meta.env.VITE_SUPABASE_URL ?? 'https://qryxnxryvjnrbgfroqrp.supabase.co'
 const supabaseKey : string = import.meta.env.VITE_API_KEY ?? import.meta.env.API_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
