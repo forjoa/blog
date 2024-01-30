75c43d6675b0a6bdb29979d110ec0dbd446b3599
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-30 13:22:48 +0100

    make mq for header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/blog-icon.png'
 import '../styles/header.css'
+import { useState } from 'react'
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <header>
       <div className='header-container'>
         <img src={logo} alt='Logo icon' />
-        <nav>
+        <nav className={`header-nav ${menuOpen ? 'menu-open' : ''}`}>
           <div className='left-nav'>
             <Link to='/'>Home</Link>
             <Link to='/search'>Search</Link>
@@ -21,6 +24,11 @@ const Header = () => {
             </Link>
           </div>
         </nav>
+        <div className='burger-menu' onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </header>
   )
