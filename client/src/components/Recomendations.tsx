c5a42fc4acde4842587d991f2ce1d2d1426e17dc
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-30 12:58:06 +0100

    delete useless fetching

--- client/src/components/Recomendations.tsx
+++ client/src/components/Recomendations.tsx
@@ -1,22 +1,5 @@
-import { useEffect } from 'react'
-import { supabase } from './database/supabase.ts'
 
 const Recomendations = () => {
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const { data, error } = await supabase.from('blogs').select('*')
-
-        if (error) throw console.log(error)
-
-        console.log(data)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-
-    fetchData()
-  }, [])
   return <></>
 }
 
