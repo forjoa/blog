2369a3dcccde25fb80b9902896dd997b3b4fda28
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-31 13:52:26 +0100

    add styles to recommendations

--- client/src/components/Recomendations.tsx
+++ client/src/components/Recomendations.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Blog } from '../types/types.ts'
 import { fetchRecommendations } from '../utils/fetchRecommendations.ts'
+import '../styles/recommendations.css'
 
 const Recomendations = () => {
   const [recommendations, setRecommendations] = useState<Blog[]>([])
 
-  fetchRecommendations().then(d => setRecommendations(d.data as Blog[]))
+  useEffect(() => {
+    fetchRecommendations().then((d) => setRecommendations(d.data as Blog[]))
+  }, [])
 
   return (
     <main className='recommendations-container'>
-      {recommendations.map((blog, index) => {
+      {recommendations?.map((blog, index) => {
         return (
           <div className='blog-recommended' key={index}>
             <h3>{blog.title}</h3>
