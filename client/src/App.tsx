e839d3fc1ac6a4a06d3c06fa9befeda4081102e6
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-02 10:09:12 +0100

    fix: add correct element to dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 // components
+import Dashboard from './components/Dashboard'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -15,7 +16,7 @@ function App() {
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/search' element={<Search />}></Route>
-        <Route path='/dashboard' element={<Search />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
       </Routes>
     </BrowserRouter>
   )
